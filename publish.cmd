20b28bea2a5d40fb5889a5a682b6b7f36d34b965
VictorNS <VictorNS@gmail.com>
2025-05-10 12:21:59 +0300

    publish

---
+++ publish.cmd
@@ -0,0 +1,2 @@
+﻿REM https://learn.microsoft.com/en-us/dotnet/core/deploying/single-file/overview?tabs=cli
+dotnet publish --verbosity normal --output c:\GitHUB\PhotoVideoDateTimeRenamer\publish\ --runtime win-x64 --self-contained false -p:PublishSingleFile=true
